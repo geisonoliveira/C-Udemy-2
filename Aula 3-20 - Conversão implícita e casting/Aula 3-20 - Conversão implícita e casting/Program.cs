50d6a10ea4a55afaae9247391c24136526a8748a
Geison Oliveira <gs_gaara@hotmail.com>
2022-07-14 21:48:52 -0300

    Aula 3-20 Conversão implicita e casting

---
+++ Aula 3-20 - Conversão implícita e casting/Aula 3-20 - Conversão implícita e casting/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+float x = 4.5f;
+
+double y = x;
+
+Console.WriteLine(y);
+
+double a;
+float b;
+
+a = 5.1;
+b = (float)a;
+
+Console.WriteLine(b);
+
+int c;
+c = (int)a;
+
+Console.WriteLine(c);
+
+int d = 5;
+int e = 2;
+
+double resultado = (double) d / e;
+
+Console.WriteLine(resultado);
