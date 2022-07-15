5b2bbf081e6ed2563604d9b96d9e982b930267b6
Geison Oliveira <gs_gaara@hotmail.com>
2022-07-14 21:39:26 -0300

    Aula 3-19

---
+++ Aula 3-19/Aula 3-19/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 10;
+Console.WriteLine(a);
+
+a += 2;
+Console.WriteLine(a);
+
+a *= 3;
+Console.WriteLine(a);
+
+string s = "ABC";
+Console.WriteLine(s);
+
+s += "DEF";
+Console.WriteLine(s);
+
+int b = 10;
+b++;
+Console.WriteLine(b);
+
+b--;
+Console.WriteLine(b);
+
+int c = 10;
+int d = c++;
+Console.WriteLine(c);
+Console.WriteLine(d);
+
+int e = 10;
+int f = ++e;
+Console.WriteLine(e);
+Console.WriteLine(f);
